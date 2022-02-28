dd76b66987d7a76fd7ed572a8bbed60ddb2537b3
iinaamasum <iinaamasum@gmail.com>
2022-03-01 00:09:15 +0600

    table__responsive

--- js/app.js
+++ js/app.js
@@ -26,13 +26,13 @@ const showPhoneInCard = (phones) => {
       cardDiv.classList.add('col');
       cardDiv.innerHTML = `
         <div class="card h-100 shadow">
-          <img src="${phone.image}" class="card-img-top" alt="...">
+          <img src="${phone?.image}" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Phone Title: ${phone.phone_name}</h5>
-            <h5 class="card-title">Brand Name: ${phone.brand}</h5>
+            <h5 class="card-title">Phone Title: ${phone?.phone_name}</h5>
+            <h5 class="card-title">Brand Name: ${phone?.brand}</h5>
           </div>
           <div class="card-footer bg-white">
-          <button onclick="PhoneDetails('${phone.slug}')" class="btn btn-outline-primary col-12 shadow-none">More Details</button>
+          <button onclick="PhoneDetails('${phone?.slug}')" class="btn btn-outline-primary col-12 shadow-none">More Details</button>
           </div>
         </div>`;
       document.getElementById('new__card').appendChild(cardDiv);
@@ -44,7 +44,7 @@ const PhoneDetails = (id) => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((info) => ReadAllData(info.data));
+    .then((info) => ReadAllData(info?.data));
 };
 
 const ReadAllData = (data) => {
@@ -55,14 +55,15 @@ const ReadAllData = (data) => {
   imgDiv.classList.add('mt-5');
   imgDiv.classList.add('col-md-5');
   imgDiv.innerHTML = `
-  <img class="img-fluid col-12 pt-5" src="${data.image}" alt="" srcset="">
-  <h4 class="text-center py-3 text-danger">Picture: ${data.name}</h4>
+  <img class="img-fluid col-12 pt-5" src="${data?.image}" alt="" srcset="">
+  <h4 class="text-center py-3 text-danger">Picture: ${data?.name}</h4>
   `;
   document.getElementById('details__add').appendChild(imgDiv);
 
   const detailsDiv = document.createElement('div');
   detailsDiv.classList.add('col-12');
   detailsDiv.classList.add('col-md-7');
+  detailsDiv.classList.add('table-responsive');
   detailsDiv.innerHTML = `
         <table class="table table-striped table-hover">
           <thead class="text-center">
@@ -74,35 +75,35 @@ const ReadAllData = (data) => {
           <tbody>
             <tr>
               <td class="col-4 fs-5 text-center text-primary">Unique Id</td>
-              <td class="col-8">${data.slug}</td>
+              <td class="col-8">${data?.slug}</td>
             </tr>
             <tr>
               <td class="col-4 fs-5 text-center text-success">Brand</td>
-              <td class="col-8">${data.brand}</td>
+              <td class="col-8">${data?.brand}</td>
             </tr>
             <tr>
               <td class="col-4 fs-5 text-center text-primary">Phone Name</td>
-              <td class="col-8">${data.name}</td>
+              <td class="col-8">${data?.name}</td>
             </tr>
             <tr>
               <td class="col-4 fs-5 text-center text-success">Display</td>
-              <td class="col-8">${data.mainFeatures.displaySize}</td>
+              <td class="col-8">${data?.mainFeatures?.displaySize}</td>
             </tr>
             <tr>
               <td class="col-4 fs-5 text-center text-primary">Storage</td>
-              <td class="col-8">${data.mainFeatures.storage}</td>
+              <td class="col-8">${data?.mainFeatures?.storage}</td>
             </tr>
             <tr>
               <td class="col-4 fs-5 text-center text-success">Memory</td>
-              <td class="col-8">${data.mainFeatures.memory}</td>
+              <td class="col-8">${data?.mainFeatures?.memory}</td>
             </tr>
             <tr>
               <td class="col-4 fs-5 text-center text-primary">Chipset</td>
-              <td class="col-8">${data.mainFeatures.chipSet}</td>
+              <td class="col-8">${data?.mainFeatures?.chipSet}</td>
             </tr>
             <tr>
               <td class="col-4 fs-5 text-center text-success">Sensors</td>
-              <td class="col-8">${data.mainFeatures.sensors}</td>
+              <td class="col-8">${data?.mainFeatures?.sensors}</td>
             </tr>
             <tr>
               <td class="col-4 fs-5 text-center text-success">Release</td>
@@ -122,27 +123,27 @@ const ReadAllData = (data) => {
           <tbody>
             <tr>
               <td class="col-4">WLAN</td>
-              <td class="col-8">${data.others.WLAN}</td>
+              <td class="col-8">${data?.others?.WLAN}</td>
             </tr>
             <tr>
               <td class="col-4">Bluetooth</td>
-              <td class="col-8">${data.others.Bluetooth}</td>
+              <td class="col-8">${data?.others?.Bluetooth}</td>
             </tr>
             <tr>
               <td class="col-4">GPS</td>
-              <td class="col-8">${data.others.GPS}</td>
+              <td class="col-8">${data?.others?.GPS}</td>
             </tr>
             <tr>
               <td class="col-4">NFC</td>
-              <td class="col-8">${data.others.NFC}</td>
+              <td class="col-8">${data?.others?.NFC}</td>
             </tr>
             <tr>
               <td class="col-4">Radio</td>
-              <td class="col-8">${data.others.Radio}</td>
+              <td class="col-8">${data?.others?.Radio}</td>
             </tr>
             <tr>
               <td class="col-4">USB</td>
-              <td class="col-8">${data.others.USB}</td>
+              <td class="col-8">${data?.others?.USB}</td>
             </tr>
               
            </tbody>
